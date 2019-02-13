137b022356d29c3831fbca3299dd26ae1c393d77
ryder-mcdowell <ryder.mcdowell@gmail.com>
2019-02-12 23:07:19 -0800

    Add cors module

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 var express = require('express');
-var app = express();
+var cors = require('cors');
 var AWS = require('aws-sdk');
+
+var app = express();
 var s3 = new AWS.S3();
+
+app.use(cors());
 
 app.get('/', function(req, res) {
    res.send('Welcome to Spooterfy')
